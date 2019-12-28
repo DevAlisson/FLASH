36d6b7096ae7eb9be7e645d14a448cb1577a0f77
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2019-12-27 22:11:08 -0300

    Delete ping.js

--- Commands/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, message) => {
- var m = await message.channel.send(`Calculando...`);
- m.edit(`Concluído! Latência é ${m.createdTimestamp - message.createdTimestamp}ms` +
- ` API Latência é ${Math.round(client.ping)}ms`);
-};

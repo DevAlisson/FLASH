594a1ea9af81c69d44fb0e7511eaef3c2c74083d
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2019-12-27 21:41:31 -0300

    Inicio de Tudo

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js"); //baixar a lib
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
+
+
+client.on("ready", () => {
+  console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
+  client.user.setPresence({ game: { name: 'comando', type: 1, url: 'https://www.twitch.tv/pedroricardo'} });
+    //0 = Jogando
+    //  1 = Transmitindo
+    //  2 = Ouvindo
+    //  3 = Assistindo
+});
+
+client.on("message", async message => { 
+    if(message.author.bot) return;
+    if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
+        return message.reply("Olá meu prefixo é `-`")}
+    if(!message.content.startsWith(config.prefix)) return;
+
+let args = message.content.split(" ").slice(1);
+let command = message.content.split(" ")[0];
+command = command.slice(config.prefix.length);
+  try {
+      let commandFile = require(`./commands/${command}.js`);
+      delete require.cache[require.resolve(`./commands/${command}.js`)];
+      return commandFile.run(client, message, args);
+  } catch (err) {
+        console.error("Erro:" + err)
+  }
+})
+
+client.login(config.token);

c2d52d4d575c82ff360e78f0b51ea406ec191d56
devrafinha <59298050+devrafinha@users.noreply.github.com>
2019-12-27 21:43:36 -0300

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,1 +1,5 @@
-tyeste
+module.exports = async (client, message) => {
+ var m = await message.channel.send(`Calculando...`);
+ m.edit(`Concluído! Latência é ${m.createdTimestamp - message.createdTimestamp}ms` +
+ ` API Latência é ${Math.round(client.ping)}ms`);
+};

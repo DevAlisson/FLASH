0a91577183b9aeb1b6a3c14d909b66760723cee5
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-10-02 15:34:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = { // Exportação das Configurações
-    TOKEN: 'NjYwMjY2ODE0OTQwMzgxMTg0.XgaYAw.hAqzpjzjxg2u3GXg9fLvRa81ojs', // Este é o lugar onde fica o TOKEN do bot.
+    TOKEN: 'TOKEN DO BOT', // Este é o lugar onde fica o TOKEN do bot.
     PREFIX: 'f!' // Prefix padrão do bot.
 }
